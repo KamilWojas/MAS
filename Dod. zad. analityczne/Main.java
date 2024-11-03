940d5f3e8c28592688936e2923754d2ff70de10e
Kamil Wojas <107795476+KamilWojas@users.noreply.github.com>
2024-11-03 17:48:13 +0100

    Add files via upload

---
+++ Dod. zad. analityczne/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+/*
+        Machine m = new Car(4);
+        Car c = new Car("Seicento",100,6);
+        System.out.println(c);
+        System.out.println(m);
+        ExperimentalDevice ex = new ExperimentalDevice(7);
+        System.out.println(ex.getName());
+        Car sc = new RaceCar(4);
+        sc.setNumberOfTires(4);
+        System.out.println(sc);
+        RaceCar rc = new RaceCar("McQueen",260,6);
+        rc.toString();
+        System.out.println(rc.toString());
+        c.setPower(5);
+        rc.setPower(260);
+*/
+
+    }
+
+}
---
+++ Dod. zad. analityczne/RaceCar.java
@@ -0,0 +1,34 @@
+public class RaceCar extends Car{
+
+    public RaceCar(int numberOfTires) {
+        super(numberOfTires);
+    }
+
+    public RaceCar(String name, int power, int numberOfTires) {
+        super(name, power, numberOfTires);
+    }
+
+    @Override
+    public String getName() {
+        return "Supercool "+super.getName();
+    }
+
+    @Override
+    public int getPower() {
+        return 10*super.getPower();
+    }
+
+    @Override
+    public void setNumberOfTires(int numberOfTires) {
+        if (numberOfTires != getNumberOfTires()){
+            System.out.println("Bad Idea");
+        }
+
+        super.setNumberOfTires(numberOfTires);
+    }
+
+    @Override
+    public String toString() {
+        return "Superfast " + getName();
+    }
+}
